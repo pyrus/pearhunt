eb6c4e30c8268656d25ee26b01aab30e87405552
Brett Bieber <brett.bieber@gmail.com>
2011-05-12 14:09:53 -0500

    Minor improvements to the scripts and add upgrade script which sets up the database

---
+++ upgrade.php
@@ -0,0 +1,32 @@
+<?php
+if (file_exists(__DIR__ . '/config.inc.php')) {
+    require_once __DIR__ . '/config.inc.php';
+} else {
+    require __DIR__ . '/config.sample.php';
+}
+
+echo 'Connecting to the database&hellip;'.PHP_EOL;
+$db = Record::getDB();
+echo 'connected successfully!<br />'.PHP_EOL;
+/**
+ * 
+ * Enter description here ...
+ * @param mysqli $db
+ * @param $sql
+ * @param $message
+ * @param $fail_ok
+ */
+function exec_sql($db, $sql, $message, $fail_ok = false)
+{
+    echo $message.'&hellip;'.PHP_EOL;
+    $result = $db->multi_query($sql);
+    if (!$result) {
+        echo 'The query failed: ';
+        echo $mysqli->error;
+        exit(1);
+    }
+    echo 'finished.<br />'.PHP_EOL;
+}
+exec_sql($db, file_get_contents(__DIR__ . '/data/pearhunt.sql'), 'Initializing database structure');
+
+echo 'Upgrade complete!';
