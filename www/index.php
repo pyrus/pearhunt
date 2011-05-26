896e827811ce468faf7b4e1b328d252d7d63e7e2
Brett Bieber <brett.bieber@gmail.com>
2011-05-26 17:20:28 -0500

    Get basic output rendering working

---
+++ www/index.php
@@ -0,0 +1,26 @@
+<?php
+if (file_exists(dirname(__DIR__).'/config.inc.php')) {
+    require_once dirname(__DIR__).'/config.inc.php';
+} else {
+    require_once dirname(__DIR__).'/config.sample.php';
+}
+
+$request = new Request($_GET, $_POST, $_FILES);
+
+$controller = new Controller($request);
+
+$savvy = new PEAR2\Templates\Savant\Main();
+$savvy->addTemplatePath(dirname(__FILE__).'/templates/html');
+
+switch ($request->format) {
+	case 'json':
+		header('Content-type:application/json');
+        $savvy->addTemplatePath(dirname(__FILE__).'/templates/'.$request->format);
+	    break;
+    default:
+        header('Content-type:text/html;charset=UTF-8');
+}
+
+$savvy->addGlobal('controller', $controller);
+$savvy->addGlobal('request', $request);
+echo $savvy->render($controller);
