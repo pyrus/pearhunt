1e7cd72a70b2abfe566285b4149a8fdc7b6deb7b
Brett Bieber <brett.bieber@gmail.com>
2011-05-26 22:45:57 -0500

    Move all the pyrus logic into the updateChannel script

--- scripts/updateChannel.php
+++ scripts/updateChannel.php
@@ -14,14 +14,39 @@ if (!isset($_SERVER['argv'], $_SERVER['argv'][1])
     exit(1);
 }
 
-$channel = Channel::getByName($_SERVER['argv'][1]);
+if (!isset($channel)) {
+    $channel = Channel::getByName($_SERVER['argv'][1]);
+}
 
 if (false === $channel) {
     echo "That channel does not exist!\n";
     exit(1);
 }
 
-foreach ($channel->getPackages() as $package) {
-	$package->save();
+$channel_file = new \PEAR2\Pyrus\ChannelFile($channel->name, false, true);
+$pyrus_channel = new \PEAR2\Pyrus\Channel($channel_file);
+$config = \PEAR2\Pyrus\Config::current();
+
+// Ensure the channel currently exists in the registry
+if (!$config->channelregistry->exists($pyrus_channel->name)) {
+    $config->channelregistry->add($pyrus_channel);
+}
+
+$packages = new \PEAR2\Pyrus\Channel\RemotePackages($config->channelregistry[$pyrus_channel->name]);
+
+
+foreach ($packages as $remote_package) {
+	// Check if we know about this package already
+    if (false === $package = Package::getByChannelAndName($channel, (string)$remote_package->name)) {
+        $package = new Package();
+    }
+
+    // Update all the info
+    $package->channel_id  = $channel->id;
+    $package->name        = $remote_package->name;
+    $package->description = $remote_package->description;
+    if ($package->save()) {
+    	echo ' -'.$channel->name.'/'.$remote_package->name.PHP_EOL;
+    }
 }
 
