f1f3f67d514c5dde9b36816cd7667e6ab80f2328
till <till@php.net>
2011-07-12 00:19:35 +0200

    * only add WHERE clause with LIKE queries when an option is set

--- src/PackageSearch.php
+++ src/PackageSearch.php
@@ -7,9 +7,19 @@ class PackageSearch extends Packages
     function __construct($options)
     {
         $this->options = $options + $this->options;
-        $records = array();
-        $mysqli = Record::getDB();
-        $sql = 'SELECT packages.* FROM packages WHERE name LIKE "%'.$mysqli->escape_string($options['q']).'%" OR description LIKE "%'.$mysqli->escape_string($options['q']).'%"';
+        $records       = array();
+        $mysqli        = Record::getDB();
+
+        $sql  = 'SELECT packages.* FROM packages';
+
+        if (isset($options['q']) && !empty($options['q'])) {
+            $sql .= ' WHERE name LIKE "%';
+            $sql .= $mysqli->escape_string($options['q']);
+            $sql .= '%" OR description LIKE "%';
+            $sql .= $mysqli->escape_string($options['q']);
+            $sql .= '%"';
+        }
+
         if ($result = $mysqli->query($sql)) {
             $records = $result->fetch_all(MYSQL_ASSOC);
         }
