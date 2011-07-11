6fa82e60e0d9167384e836dd589ae0b60068ac10
till <till@php.net>
2011-07-12 00:47:58 +0200

    php_eol

--- scripts/addChannel.php
+++ scripts/addChannel.php
@@ -8,19 +8,19 @@ if (file_exists(dirname(__FILE__).'/../etc/config.inc.php')) {
 
 if (!isset($_SERVER['argv'], $_SERVER['argv'][1])
     || $_SERVER['argv'][1] == '--help' || $_SERVER['argc'] != 2) {
-    echo "This program requires 1 argument.\n";
-    echo "addChannel.php channel\n\n";
-    echo "Example: addChannel.php pear.php.net\n";
+    echo "This program requires 1 argument." . PHP_EOL;
+    echo "addChannel.php channel" . PHP_EOL . PHP_EOL;
+    echo "Example: addChannel.php pear.php.net" . PHP_EOL;
     exit(1);
 }
 
 if ($channel = Channel::getByName($_SERVER['argv'][1])) {
-    echo "That channel already exists!\n";
+    echo "That channel already exists!" . PHP_EOL;
 } else {
 	$channel = new \Channel();
 }
 
-echo "Adding channel...\n";
+echo "Adding channel..." . PHP_EOL;
 
 
 $pyrus_channel = new Remote_PyrusChannel($_SERVER['argv'][1]);
