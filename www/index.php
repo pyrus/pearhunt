ee6db4bdc343fddce1a8ea7d440178f3baef3145
till <till@php.net>
2011-07-12 17:06:23 +0200

    * use different model

--- www/index.php
+++ www/index.php
@@ -1,21 +1,23 @@
 <?php
 require_once __DIR__ . '/../bootstrap.php';
 
-$request = new Request($_GET, $_POST, $_FILES);
-
-$controller = new Controller($request);
+$request = new Request($_GET, $_POST, $_FILES, $_SERVER);
 
 $savvy = new PEAR2\Templates\Savant\Main();
-$savvy->addTemplatePath(dirname(__FILE__).'/templates/html');
+$savvy->addTemplatePath(__DIR__ . '/templates/html');
 
 switch ($request->format) {
 	case 'json':
 		header('Content-type:application/json');
-        $savvy->addTemplatePath(dirname(__FILE__).'/templates/'.$request->format);
+        $savvy->addTemplatePath(__DIR__ . '/templates/' . $request->format);
 	    break;
     default:
         header('Content-type:text/html;charset=UTF-8');
+        $request->setRequestedModel('Channels');
+        break;
 }
+
+$controller = new Controller($request);
 
 $savvy->addGlobal('controller', $controller);
 $savvy->addGlobal('request', $request);
