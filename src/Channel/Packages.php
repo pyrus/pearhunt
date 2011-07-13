f5e6c61d6eacf51631de55a36a445ccd6450b130
Brett Bieber <brett.bieber@gmail.com>
2011-07-13 08:04:29 -0500

    Refactor due to the changes in the Packages iterator — still not 100% functional

--- src/Channel/Packages.php
+++ src/Channel/Packages.php
@@ -1,19 +1,19 @@
 <?php
 class Channel_Packages extends Packages
 {
-    public $options = array('offset' => 0,
-                            'limit'  => -1);
+    protected $channel;
 
     function __construct(Channel $channel, $options)
     {
-        $this->options = $options + $this->options;
-        $records = array();
-        $mysqli = Record::getDB();
+        $this->channel = $channel;
+
+        parent::__construct($options);
+    }
+
+    function getSQL()
+    {
         $sql = 'SELECT packages.* FROM packages LEFT JOIN channels ON channel_id';
-        if ($result = $mysqli->query($sql)) {
-            $records = $result->fetch_all(MYSQL_ASSOC);
-        }
-        parent::__construct(new ArrayIterator($records), $this->options['offset'], $this->options['limit']);
+        return $sql;
     }
 
 }
