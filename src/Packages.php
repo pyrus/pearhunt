4b5c14a449db7d21adeb7c8ab0bacbc7c987289c
Brett Bieber <brett.bieber@gmail.com>
2011-07-13 07:55:50 -0500

    * make sure to return 'null', if there is no current element

--- src/Packages.php
+++ src/Packages.php
@@ -3,7 +3,11 @@ abstract class Packages extends DBResultIterator
 {
 	function current()
 	{
-		$data    = parent::current();
+		$data = parent::current();
+		if ($data === null) {
+		    return null;
+		}
+
 		$package = new Package();
 		$package->synchronizeWithArray($data);
 		return $package;
