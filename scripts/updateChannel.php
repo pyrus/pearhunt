fed8b38fcd9244ee81916753a862dd57fa9b838c
Brett Bieber <brett.bieber@gmail.com>
2011-09-09 14:43:11 -0500

    This script can be used as a pass-thru, so check if $channel isn't set.

--- scripts/updateChannel.php
+++ scripts/updateChannel.php
@@ -2,8 +2,9 @@
 <?php
 require_once __DIR__ . '/../bootstrap.php';
 
-if (!isset($_SERVER['argv'], $_SERVER['argv'][1])
-    || $_SERVER['argv'][1] == '--help' || $_SERVER['argc'] != 2) {
+if (!isset($channel) && (
+    !isset($_SERVER['argv'], $_SERVER['argv'][1])
+    || $_SERVER['argv'][1] == '--help' || $_SERVER['argc'] != 2)) {
     echo "This program requires 1 argument.\n";
     echo "updateChannel.php channel\n\n";
     echo "Example: updateChannel.php pear.php.net\n";
