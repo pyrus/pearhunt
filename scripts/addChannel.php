52a022d195942d33a603319250462c743da2f230
Brett Bieber <brett.bieber@gmail.com>
2011-09-09 15:02:43 -0500

    Correct example for adding a channel and improve check for existing channels.

--- scripts/addChannel.php
+++ scripts/addChannel.php
@@ -6,19 +6,19 @@ if (!isset($_SERVER['argv'], $_SERVER['argv'][1])
     || $_SERVER['argv'][1] == '--help' || $_SERVER['argc'] != 2) {
     echo "This program requires 1 argument." . PHP_EOL;
     echo "addChannel.php channel" . PHP_EOL . PHP_EOL;
-    echo "Example: addChannel.php pear.php.net" . PHP_EOL;
+    echo "Example: addChannel.php http://pear.php.net/channel.xml" . PHP_EOL;
     exit(1);
 }
 
-if ($channel = Channel::getByName($_SERVER['argv'][1])) {
+$channel_file = new \PEAR2\Pyrus\ChannelFile($_SERVER['argv'][1], false, true);
+
+if ($channel = Channel::getByName($channel_file->name)) {
     echo "That channel already exists!" . PHP_EOL;
 } else {
 	$channel = new \Channel();
 }
 
 echo "Adding channel..." . PHP_EOL;
-
-$channel_file = new \PEAR2\Pyrus\ChannelFile($_SERVER['argv'][1], false, true);
 
 $channel->name        = $channel_file->name;
 $channel->alias       = $channel_file->alias;
